9508d49d1486675cfdf9edfa6677825cf3ec7de3
gilbertKorir <jerikorir@gmail.com>
2026-01-02 10:59:05 +0300

    updates

--- Happy Birthday_files/script.js
+++ Happy Birthday_files/script.js
@@ -20,6 +20,26 @@ $('#play').click(function () {
     audio.play();
 
 });
+
+// $('#play').click(function () {
+//     $(".loader").fadeOut("slow");
+//     sf.destroy();
+
+//     $('.balloon-border').animate({
+//         top: -500
+//     }, 8000);
+
+//     var audio = $('.song')[0];
+//     audio.currentTime = 0; // start from beginning
+//     audio.play();
+
+//     // ⏱ Stop after 15 seconds
+//     setTimeout(function () {
+//         audio.pause();
+//         audio.currentTime = 0; // optional: reset
+//     }, 15000);
+// });
+
 var typed = new Typed("#typed", {
     stringsElement: '#typed-strings',
     typeSpeed: 30,
